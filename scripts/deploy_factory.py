a395366ed04804220f6b0b92f4dcc0665aaa5d6e
Azat Serikov <azatsdev@gmail.com>
2023-03-09 21:52:52 +0600

    feat: deploy factory script

--- scripts/deploy_factory.py
+++ scripts/deploy_factory.py
@@ -14,9 +14,21 @@ def main():
     logger.info("Loading deployer...")
     deployer = get_deployer()
     logger.success(f"Deployer: {deployer}")
+
+    """
+        DEPLOY BLUEPRINT
+    """
     gate_seal_bytecode = project.GateSeal.contract_type.deployment_bytecode.bytecode
     blueprint_deploy_bytecode = construct_blueprint_deploy_bytecode(gate_seal_bytecode)
     verify_blueprint_deploy_preamble(blueprint_deploy_bytecode)
-    blueprint_address = deploy_blueprint(deployer, gate_seal_initcode)
+    blueprint_address = deploy_blueprint(deployer, blueprint_deploy_bytecode)
     verify_eip522_blueprint(networks.active_provider.get_code(blueprint_address))
     logger.success(f"Blueprint deployed: {blueprint_address}")
+
+    """
+        DEPLOY FACTORY
+    """
+
+    factory = project.GateSealFactory.deploy(blueprint_address, sender=deployer)
+    assert factory.get_blueprint() == blueprint_address
+    logger.success(f"Factory deployed: {factory.address}")
