8438303e3873775c7b42a32eb5eee80219af82e5
Azat Serikov <azatsdev@gmail.com>
2023-03-09 21:43:21 +0600

    feat: blueprint deploy script

---
+++ scripts/deploy_blueprint.py
@@ -0,0 +1,22 @@
+from ape.logging import logger
+from utils.blueprint import (
+    construct_blueprint_deploy_bytecode,
+    verify_blueprint_deploy_preamble,
+    verify_eip522_blueprint,
+    deploy_blueprint,
+)
+from utils.config import get_deployer
+from utils.env import load_env_variable
+from ape import project, networks
+
+
+def main():
+    logger.info("Loading deployer...")
+    deployer = get_deployer()
+    logger.success(f"Deployer: {deployer}")
+    gate_seal_bytecode = project.GateSeal.contract_type.deployment_bytecode.bytecode
+    blueprint_deploy_bytecode = construct_blueprint_deploy_bytecode(gate_seal_bytecode)
+    verify_blueprint_deploy_preamble(blueprint_deploy_bytecode)
+    blueprint_address = deploy_blueprint(deployer, gate_seal_initcode)
+    verify_eip522_blueprint(networks.active_provider.get_code(blueprint_address))
+    logger.success(f"Blueprint deployed: {blueprint_address}")
