e72f4e990bea097c52cd9bdc64e76be67bdbe4ce
Azat Serikov <azatsdev@gmail.com>
2023-05-02 11:55:10 +0600

    feat: improve gateseal check script

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,8 @@
 from ape import networks
 
 
-def construct_deployed_filename(address: str, type="gateseal") -> str:
-    return f"deployed/{networks.active_provider.network.name}/{type}/{address.lower()}.json"
+def construct_deployed_filename(address: str, type="gateseal", check=False) -> str:
+    network = networks.active_provider.network.name
+    if check:
+        network = networks.active_provider.network.name.replace("-fork", "")
+    return f"deployed/{network}/{type}/{address.lower()}.json"
